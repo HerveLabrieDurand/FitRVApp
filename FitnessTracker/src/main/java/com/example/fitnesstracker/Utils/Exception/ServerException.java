f9c5928a93ca88259b1ae58114ded2bedddba1df
Hervé Labrie-Durand <hervelabriedurand@gmail.com>
2023-10-07 19:44:57 -0400

    No more custom exception handling and relation table entity

--- FitnessTracker/src/main/java/com/example/fitnesstracker/Utils/Exception/ServerException.java
+++
@@ -1,7 +0,0 @@
-package com.example.fitnesstracker.Utils.Exception;
-
-public class ServerException extends Exception {
-    public ServerException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
